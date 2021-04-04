76b6c5c5d9792f0628f41f8665c64a72bc89b874
Ahmet Cengiz <ahmetcengiz805@hotmail.com>
2021-04-04 15:40:07 +0300

    category compenent için service yapısı oluşturuldu

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Category } from './category';
-import {HttpClient} from '@angular/common/http';
 import { CategoryService } from '../services/category.service';
 
 @Component({
@@ -10,7 +9,7 @@ import { CategoryService } from '../services/category.service';
   providers:[CategoryService]
 })
 export class CategoryComponent implements OnInit {
-  constructor(private http:HttpClient,private categoryService:CategoryService) {}
+  constructor(private categoryService:CategoryService) {}
   title = 'Kategori Listesi';
   categories: Category[];
   ngOnInit(): void {
